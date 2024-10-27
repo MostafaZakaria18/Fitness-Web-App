1d8194fe1554f0d3ba559d7a7e80376dea3ab9bb
MOSTAFAGTR <168591264+MOSTAFAGTR@users.noreply.github.com>
2024-10-27 21:59:57 +0300

    Add files via upload

---
+++ scripto.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+     const menuToggle = document.querySelector('.menu-toggle');
+      const nav = document.querySelector('nav ul'); 
+      menuToggle.addEventListener('click', function () {
+         nav.classList.toggle('active');
+         });
+         });
+         setTimeout(() => {
+            document.getElementById("welcomeOverlay").style.display = "none";
+        }, 3000);
